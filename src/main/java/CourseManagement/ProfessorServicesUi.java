e6dc3a9bab2beb9de0f7c6531d3ee50e237bba29
Nyabi🥨🍚 <121873539+nyabi021@users.noreply.github.com>
2024-12-05 01:45:03 +0900

    Update ProfessorServicesUi.java

--- src/main/java/CourseManagement/ProfessorServicesUi.java
+++ src/main/java/CourseManagement/ProfessorServicesUi.java
@@ -1,5 +1,6 @@
 package CourseManagement;
 
+import project.professor.ProfessorServiceUi; // ProfessorServiceUi를 임포트합니다.
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -11,7 +12,7 @@ import Login.ModifyUserUi;
 public class ProfessorServicesUi extends JFrame {
     public ProfessorServicesUi() {
         setTitle("교수 창");
-        setSize(400, 300);
+        setSize(400, 400); 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         
@@ -20,33 +21,28 @@ public class ProfessorServicesUi extends JFrame {
         detail.fill = GridBagConstraints.HORIZONTAL;
         detail.insets = new Insets(10, 100, 10, 100); 
         
-        // 강좌별 학생 명단 버튼
         JButton studentListButton = new JButton("강좌별 학생 명단");
         studentListButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                new StudentListUi(); // 강좌별 학생 명단 창 열기
+                new StudentListUi(); 
             }
         });
         detail.gridx = 0;
         detail.gridy = 0;
         add(studentListButton, detail);
         
-        
-        // 출석부 조회 버튼
         JButton attendanceButton = new JButton("출석부 조회");
         attendanceButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                new AttendanceUi(); // 새로운 출석부 조회 창 열기
+                new AttendanceUi(); 
             }
         });
         detail.gridx = 0;
         detail.gridy = 1;
         add(attendanceButton, detail);
         
-        
-        // 회원 정보 변경 버튼
         JButton modifyUser = new JButton("회원 정보 변경");
         modifyUser.addActionListener(new ActionListener() {
             @Override
@@ -55,8 +51,19 @@ public class ProfessorServicesUi extends JFrame {
              }
         });
         detail.gridx = 0;
-        detail.gridy = 3;
+        detail.gridy = 2;
         add(modifyUser, detail);
+        
+        JButton scoreManagementButton = new JButton("점수 관리");
+        scoreManagementButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ProfessorServiceUi(); 
+            }
+        });
+        detail.gridx = 0;
+        detail.gridy = 3;
+        add(scoreManagementButton, detail);
 
         setVisible(true);
     }
